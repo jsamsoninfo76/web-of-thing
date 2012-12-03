9f7429e52a22d071e0cd70dd9dcb6ca384735cbe
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@5ba9d220-ea5e-83a3-9bb3-bf5aa7f489ca>
2012-12-03 19:15:43 +0000

    Fin leds

--- js/script.js
+++ js/script.js
@@ -1,43 +1,78 @@
-var leds = new Array(0,0,0,0); 
+var nb_led = 6;
 
-/*function ajaxGet(url,nled,callBack,arg) {*/
-function ajaxGet(nled) {
-	var xhr;
-	var url;
-	var mbed;
-
-	//if (leds[nled] == 0) leds[nled] = 1;
-	//else leds[nled] = 0;
+function init(){
+	//Declaration des left des différentes leds 
+	var current_coord = new Array(0,0);
+	var current_size = new Array(0,0);
+	var led_left = new Array(21,48,75,101,100,100);
+	var led_top = new Array(263,263,263,263,36,135);
+	var target;
 	
-	//Changement de l'état des LED
-	leds[nled] = (leds[nled] == 0) ? leds[nled] = 1 : leds[nled] = 0;
-	
-	//Generation du code HTML pour supperposition d'image
-	mbed = document.getElementById("mbed");
-	mbed.innerHTML = '<img id=fond src=img/mbed.jpg usemap=#leds>';
-	for (var i=0 ; i<leds.length ; i++){
-		if (leds[i] == 1)
-			mbed.innerHTML += '<div width=23 height=10><a href=javascript:ajaxGet(' + i + ')><img id=led' + i + ' src="img/led_on.jpg"></a></div>';
-
+	for (var i=0 ; i<nb_led; i++){
+		if (i<4)
+		{
+			current_size[0] = 25;
+			current_size[1] = 10;
+			target = "mbed";
+		}
+		else
+		{
+			current_size[0] = 24
+			current_size[1] = 19;
+			target = "ethernet";	
+		}
+		
+		//alert('i:'+i+ ' left:' +(current_coord[0] +led_left[i])+  ' top'  + (current_coord[1] + led_top[i]) + '  width: ' + current_size[0] + 'height: ' + current_size[1] );
+		current_coord = getPosition(target);
+		
+		/* Parametre de l'image */
+		document.getElementById("led"+i).src = "img/led_on.jpg";
+		document.getElementById("led"+i).style.position = "absolute";
+		document.getElementById("led"+i).style.left = (current_coord[0] +led_left[i]) + "px";
+		document.getElementById("led"+i).style.top = (current_coord[1] + led_top[i]) + "px";
+		document.getElementById("led"+i).style.width = current_size[0]+"px";
+		document.getElementById("led"+i).style.height = current_size[1]+"px";
+		
+		ajaxGet(i,'&init=1');
 	}
-	mbed.innerHTML += '<map name=leds>';
-	mbed.innerHTML += '<area shape="rect" coords="20,260,45,275" href="javascript:ajaxGet(0)" alt="led1"/>';
-	mbed.innerHTML += '<area shape="rect" coords="48,260,73,275" href="javascript:ajaxGet(1)" alt="led2"/>';
-	mbed.innerHTML += '<area shape="rect" coords="75,260,100,275" href="javascript:ajaxGet(2)" alt="led3"/>';	
-	mbed.innerHTML += '<area shape="rect" coords="101,260,126,275" href="javascript:ajaxGet(3)" alt="led4"/>';	
-	mbed.innerHTML += '</map>';
+}
+
+function ajaxGet(nled,arg) {
+	var xhr;
+	var url = (arg) ? 'led?nled='+nled+arg : 'led?nled='+nled; 
 	
-	url = 'led?nled=' + nled + '&etat=' + leds[nled]; 
+	try{ 		xhr = new XMLHttpRequest(); 					}	
+	catch(e){ 	xhr = new ActiveXObject('Microsoft.XMLHTTP'); 	}
 	
-	/*try{ 
-		//Test sur la création d'une requête HTTP
-		xhr = new XMLHttpRequest();
-	}	
-	catch(e){ 
-		xhr = new ActiveXObject('Microsoft.XMLHTTP'); 	
-	}*/
-	
-	//Ouverture et envoie de la requête en GET
-	//xhr.open("GET", url, true);
-	//xhr.send(null);
+	xhr.onreadystatechange = function(){
+		setImage(xhr,nled);
+	};
+	xhr.open("GET", url, true);
+	xhr.send(null);
+}
+
+function setImage(xhr,nled)
+{
+	if(xhr.readyState == 4 && xhr.status == 200)
+	{	
+		var etat = xhr.responseText;
+		document.getElementById("dled"+nled).style.display = (etat == '1') ? "block" : "none";
+	}
+}
+
+function getPosition(element)
+{
+	var left = 0;
+	var top = 0;
+	/*On récupère l'élément*/
+	var e = document.getElementById(element);
+	/*Tant que l'on a un élément parent*/
+	while (e.offsetParent != undefined && e.offsetParent != null)
+	{
+		/*On ajoute la position de l'élément parent*/
+		left += e.offsetLeft + (e.clientLeft != null ? e.clientLeft : 0);
+		top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+		e = e.offsetParent;
+	}
+	return new Array(left,top);
 }
